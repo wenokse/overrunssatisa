ba1fe5a9ff19eabbd8b402b666eda1da0f16b83b
wenokse <rowensecuya17@gmail.com>
2024-07-06 14:27:06 +0800

    Database com

--- includes/conn.php
+++ includes/conn.php
@@ -2,9 +2,9 @@
 
 Class Database{
  
-	private $server = "mysql:host=localhost;dbname=u510162695_ecomm";
+	private $server = "mysql:host=77.37.35.3;dbname=u510162695_ecomm";
 	private $username = "root";
-	private $password = "";
+	private $password = "1RootEcomm";
 	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
 	protected $conn;
  	
