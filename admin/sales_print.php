2a0cf4203e5d16c34b4eed4aea1783ef741fe8cf
wenokse <rowensecuya17@gmail.com>
2024-07-17 09:15:36 +0800

    comm

--- admin/sales_print.php
+++ admin/sales_print.php
@@ -63,7 +63,7 @@
 	    $content = '';  
 
 	    // Add image
-	    $content .= '<img src="../images/LOGO.png" width="200" height="100" align="center" /><br /><br />';
+	    $content .= '<img src="../images/LOGO.png" width="400" height="150" align="center" /><br /><br />';
 	    $content .= '<h4 align="center">SALES REPORT</h4>';
 	    $content .= '<h4 align="center">'.$from_title." - ".$to_title.'</h4>';
 	    $content .= '<table border="1" cellspacing="0" cellpadding="3">  
