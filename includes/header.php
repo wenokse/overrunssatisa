bd5853e14dfb995c969d130f5ed06f191a3760af
wenokse <rowensecuya17@gmail.com>
2024-10-05 12:21:06 +0800

    comm

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,6 @@
   	<meta charset="utf-8">
   	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' https://code.jquery.com https://cdn.jsdelivr.net 'unsafe-inline';">
   	<title>Overruns Sa Tisa Online Shop</title>
   
   	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
@@ -38,7 +37,6 @@
 
   	<!-- Custom CSS -->
     <style type="text/css">
-
 
     /* ::-webkit-scrollbar{
       display: none;
@@ -243,7 +241,6 @@
     </style>
 
 </head>
-
 <script>
   document.addEventListener('contextmenu', function(event) {
     event.preventDefault();
@@ -275,19 +272,3 @@
   });
   
 </script>
-<?php
-  header("Content-Security-Policy: 
-    default-src 'self'; 
-    script-src 'self' https://code.jquery.com https://cdn.jsdelivr.net 'unsafe-inline'; 
-    object-src 'none'; 
-    connect-src 'self'; 
-    style-src 'self' 'unsafe-inline'; 
-    frame-ancestors 'self'; 
-    base-uri 'self'; 
-    form-action 'self'; 
-    font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com; 
-    img-src 'self' data:; 
-    media-src 'none'; 
-    child-src 'none'; 
-    frame-src 'none';");
-?>
