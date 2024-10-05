73955e3e83193421ad3890b87ca696d6e4da708d
wenokse <rowensecuya17@gmail.com>
2024-10-05 08:12:07 +0800

    com

--- signup.php
+++ signup.php
@@ -489,7 +489,7 @@ passwordField.addEventListener('input', function() {
             passwordStrengthBar.style.backgroundColor = 'yellow'; // Stronger
             break;
         case 4:
-            passwordStrengthBar.style.backgroundColor = 'green'; // Strong
+            passwordStrengthBox.style.backgroundColor = '#39FF14'; // Strong (neon green)
             break;
     }
 });
