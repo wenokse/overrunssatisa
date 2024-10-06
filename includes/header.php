a051be9978bb7a6698b1348a9770def2eaa223c1
wenokse <rowensecuya17@gmail.com>
2024-10-06 12:51:01 +0800

    com

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
   	<meta charset="utf-8">
   	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' https://code.jquery.com https://cdn.jsdelivr.net 'unsafe-inline';">
+    <!-- <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' https://code.jquery.com https://cdn.jsdelivr.net 'unsafe-inline';"> -->
 
   	<title>Overruns Sa Tisa Online Shop</title>
   
