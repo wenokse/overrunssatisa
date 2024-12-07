eaa1534199e9be0424f5abb73d2845a60fdaa290
wenokse <rowensecuya17@gmail.com>
2024-12-07 15:50:05 +0800

    comm

--- admin/backup.php
+++ admin/backup.php
@@ -52,17 +52,27 @@
     <!-- Main content -->
     <section class="content">
       <div class="row">
-        <div class="col-md-6 col-md-offset-3">
+      <div class="col-md-15 col-md-offset-3">
           <div class="box box-solid">
             <div class="box-header with-border">
-              <h3 class="box-title">Export Your Database</h3>
+              <h3 class="box-title">Database Management</h3>
             </div>
             <div class="box-body text-center">
-              <p class="lead">Backup your database in just one click. Click the button below to export the database.</p>
-              <!-- Centering the button and giving it a more modern look -->
+              <p class="lead">Manage your database with the following options:</p>
               <a href="export_database" class="btn btn-lg btn-success">
                 <i class="fa fa-database"></i> Export Database
               </a>
+              <form action="import_database" method="post" enctype="multipart/form-data" style="margin-top: 15px;">
+                <input type="file" name="import_file" required>
+                <button type="submit" class="btn btn-lg btn-primary">
+                  <i class="fa fa-upload"></i> Import Database
+                </button>
+              </form>
+              <form action="drop_database" method="post" style="margin-top: 15px;">
+                <button type="submit" class="btn btn-lg btn-danger" onclick="return confirm('Are you sure you want to drop the database? This action is irreversible!');">
+                  <i class="fa fa-trash"></i> Drop Database
+                </button>
+              </form>
             </div>
           </div>
         </div>
